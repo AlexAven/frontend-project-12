aa6a5d8c5ad7d70a3c687729cb7b72d9756d6f5b
Alexander Kazakov <gloom31@gmail.com>
2024-12-07 17:52:49 +0300

    Step 5 v.3

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -1,13 +1,22 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Chat = () => {
+  const dispatch = useDispatch();
+
   const chatState = useSelector((state) => state.chat);
   const activeChannelIndex = chatState.ui.activeChannelIndex;
   const currentChannelId = chatState.channels.ids[activeChannelIndex];
   const currentChannel = chatState.channels.entities[currentChannelId];
-  const messagesCounter = chatState.messages.ids.filter(
+  const channelMessagesIndex = chatState.messages.ids.filter(
     (messageId) => chatState.messages.entities[messageId].channelId === currentChannelId,
-  ).length;
+  );
+  const channelMessages = channelMessagesIndex.map((id) => chatState.messages.entities[id]);
+  const messagesCounter = channelMessagesIndex.length;
+
+  // useEffect(() => {
+  //   const socket = new WebSocket();
+  // });
 
   return (
     <>
@@ -17,7 +26,19 @@ const Chat = () => {
         </p>
         <span className="text-muted">{`${messagesCounter} сообщений`}</span>
       </div>
-      <div id="messages-box" className="chat-messages overflow-auto px-5"></div>
+      <div id="messages-box" className="chat-messages overflow-auto px-5">
+        {channelMessages &&
+          channelMessages.map((message) => {
+            console.log('message', message);
+
+            const { id, username, body } = message;
+            return (
+              <div key={id} className="text-break mb-2">
+                <b>{username}</b>:{body}
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 };
