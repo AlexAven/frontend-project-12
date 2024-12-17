b5dd6964bed27146f68092f5653e243870da922b
Alexander Kazakov <gloom31@gmail.com>
2024-12-17 20:44:30 +0300

    Step# 12 v.2

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useTranslation } from 'react-i18next';
